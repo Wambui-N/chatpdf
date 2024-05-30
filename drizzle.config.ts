f1401680b52e6a56dce97ab4c0ffd42625ea9f01
Wambui Ndungu <wambuin18@gmail.com>
2024-05-30 12:20:10 +0300

    Update drizzle configuration to show expected ts error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit";
 export default defineConfig({
- schema: "./src/lib/db/schema.ts",
-  dialect: 'postgresql',
+// @ts-expect-error
+  schema: "./src/lib/db/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
-})
+});
